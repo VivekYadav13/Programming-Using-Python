0e1f4570f64fdbc0cbabefeae32203e3910b6cdf
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-02-28 22:29:51 +0530

    Update Problem-1.py

--- Practice Sheet 1/Problem-1.py
+++ Practice Sheet 1/Problem-1.py
@@ -1,13 +1,14 @@
-'''Problem 1
+'''Problem 1 (Solution 1)
 Write a program that examines three variables - x, y, and z - and prints the largest odd number among them. If none of them are odd, it should print a message to that effect.'''
 
-x, y, z = int(input("Enter first number: ")), int(input("Enter second number: ")), int(input("Enter third number: "))
+x, y, z = int(input("Enter first number: ")), int(
+    input("Enter second number: ")), int(input("Enter third number: "))
 n = 0
 if (x > y and x % 2 != 0):
     n = x
 elif (x < y and y % 2 != 0):
     n = y
-else:
+elif (x % 2 != 0):
     n = x
 if (n < z and z % 2 != 0 and n != 0):
     n = z
