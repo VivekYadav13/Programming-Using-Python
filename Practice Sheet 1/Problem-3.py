8dbf413417ab53897635df74c7823c07a363684d
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-29 10:48:16 +0530

    Update Problem-3.py

--- Practice Sheet 1/Problem-3.py
+++ Practice Sheet 1/Problem-3.py
@@ -8,6 +8,8 @@ D) sqrt(r((cos a)^2)+r((sin b)^2))
 E) (y2-y1)/(x2-x1)
 '''
 
+import math
+
 print("Part A: (3+4)(5) = ", (3+4)*(5))
 
 n = eval(input("Enter a number (n) for part B: "))
@@ -17,7 +19,9 @@ p = 3.1415  # Value of Pi
 r = eval(input("Enter a radius (r) for part C: "))
 print("Part C: 4(pi)r^2 = ", 4*(p)*(r**2))
 
-print("Part D: Multi inputs and math library is not taught yet.")
+a = float(input("Enter a number a for part D:"))
+b = float(input("Enter a number b for part D:"))
+print("Part D: sqrt(r((cos a)^2)+r((sin b)^2)) = ", ((r*(math.cos(a))**2)+(r*(math.sin(b))**2))**(1/2))
 
 x1, x2, y1, y2 = eval(input("Enter a value of x1 for part E: ")), eval(input("Enter a value of x2 for part E: ")), eval(input("Enter a value of y1 for part E: ")), eval(input("Enter a value of y2 for part E: "))
 print("Part E: (y2-y1)/(x2-x1) = ", (y2-y1)/(x2-x1))
