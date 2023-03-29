85bb7ad1d44b0a877ed7c3e1a6b53bda92661684
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-29 15:32:29 +0530

    Create Problem-3.py

---
+++ Practice Sheet 4/Problem-3.py
@@ -0,0 +1,18 @@
+"""
+Write a function called sum_digits that is given an integer num and returns the sum of the
+digits of num.
+"""
+def sum_digit(n):
+    s = 0
+    while (n > 0):
+        r = n % 10
+        s = s+r
+        n = int(n/10)
+    return s
+
+
+num = int(input("Input positive integer number: "))
+print("Sum of digits of ", num, "= ", sum_digit(num))
+while (num > 0):
+    print("Sum of digits of ", num, "= ", sum_digit(num))
+    num = int(input("Input positive integer number: "))
