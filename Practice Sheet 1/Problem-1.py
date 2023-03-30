24f529c378d7cec0d89b4aee8269ebb797e23972
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-31 00:04:10 +0530

    Update Problem-1.py

--- Practice Sheet 1/Problem-1.py
+++ Practice Sheet 1/Problem-1.py
@@ -1,5 +1,4 @@
 '''
-Problem 1:
 Write a program that examines three variables - x, y, and z - and prints the largest odd number among them. If none of them are odd, it should print a message to that effect.
 '''
 
