e0f892ebf1f4a53be00e355dea6d2bb322fd2146
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-31 00:10:12 +0530

    Update Problem-2.py

--- Practice Sheet 4/Problem-2.py
+++ Practice Sheet 4/Problem-2.py
@@ -1,7 +1,7 @@
-"""
-Fibonacci Numbers. The Fibonacci number sequence is 1, 1, 2, 3, 5, 8, 13, 21, etc. In other words, the next value of the sequence is the sum of the previous two values in the sequence. Write a routine that, given N, displays the value of the Nth Fibonacci number.
-For example, the first Fibonacci number is 1, the 6th is 8, and so on. Write a function to calculate nth Fibonacci number.
-"""
+'''
+Fibonacci Numbers. The Fibonacci number sequence is 1, 1, 2, 3, 5, 8, 13, 21, etc. In other words, the next value of the sequence is the sum of the previous two values in the sequence. Write a routine that, given N, displays the value of the Nth Fibonacci number. For example, the first Fibonacci number is 1, the 6th is 8, and so on. Write a function to calculate nth Fibonacci number.
+'''
+
 def fibonacci(n):
     f = 1
     s = 1
