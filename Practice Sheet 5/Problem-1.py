7da333a13a22208167569171400defbd8268880e
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-31 00:12:23 +0530

    Update Problem-1.py

--- Practice Sheet 5/Problem-1.py
+++ Practice Sheet 5/Problem-1.py
@@ -1,6 +1,7 @@
-"""
+'''
 People often forget closing parentheses when entering formulas. Write a program that asks the user to enter a formula and prints out whether the formula has the same number of opening and closing parentheses.
-"""
+'''
+
 a = str(input('Enter your equation: '))
 opennum = 0
 closenum = 0
