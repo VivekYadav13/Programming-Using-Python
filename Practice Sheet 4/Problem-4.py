7050284eefeaf0b049779a37aab5bd39c0c43c10
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-31 00:11:18 +0530

    Update Problem-4.py

--- Practice Sheet 4/Problem-4.py
+++ Practice Sheet 4/Problem-4.py
@@ -1,9 +1,7 @@
-"""
-The digital root of a number n is obtained as follows: Add up the digits n to get a new number. Add up the digits of that to get another new number. Keep doing this until you get a number that has only one digit. That number is the digital root.
-For example, if n = 45893, we add up the digits to get 4 + 5 + 8 + 9 + 3 = 29.
-We then add up the digits of 29 to get 2 + 9 = 11. We then add up the digits of 11 to get 1 + 1 = 2. Since 2 has only one digit, 2 is our digital root.
-Write a function that returns the digital root of an integer n.
-"""
+'''
+The digital root of a number n is obtained as follows: Add up the digits n to get a new number. Add up the digits of that to get another new number. Keep doing this until you get a number that has only one digit. That number is the digital root. For example, if n = 45893, we add up the digits to get 4 + 5 + 8 + 9 + 3 = 29. We then add up the digits of 29 to get 2 + 9 = 11. We then add up the digits of 11 to get 1 + 1 = 2. Since 2 has only one digit, 2 is our digital root. Write a function that returns the digital root of an integer n.
+'''
+
 def sum_digit(n):
     s = 0
     while (n > 0):
