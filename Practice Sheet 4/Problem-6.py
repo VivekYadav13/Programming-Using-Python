7642f2041728995636ec5b0a3d3486ac48d8b9d3
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-03-31 16:19:29 +0530

    Update Problem-6.py

--- Practice Sheet 4/Problem-6.py
+++ Practice Sheet 4/Problem-6.py
@@ -1,7 +1,5 @@
 '''
-Write a function that takes an integer n and returns a random integer with exactly n digits. For
- instance, if n is 3, then 125 and 593 would be valid return values, but 093 would not because
- that is really 93, which is a two-digit number.
+Write a function that takes an integer n and returns a random integer with exactly n digits. For instance, if n is 3, then 125 and 593 would be valid return values, but 093 would not because that is really 93, which is a two-digit number.
 '''
 
 from random import randint
