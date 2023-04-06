a057198c686c9c22f89ff8acfbb57ce98c3a8908
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-06 09:46:42 +0530

    Create Problem-02.py

---
+++ Practice Sheet 07/Problem-02.py
@@ -0,0 +1,12 @@
+a = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+b = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
+d = []
+for i in range(len(a)):
+    c = []
+    for j in range(len(a[0])):
+        t = 0
+        for k in range(len(b[0])):
+            t += a[i][k] * b[k][j]
+        c += [t]
+    d = d + [tuple(c)]
+print(tuple(d))
