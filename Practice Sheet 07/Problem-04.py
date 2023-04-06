c19ef1e0772ef0e988e7b4cdcc2274b85badddba
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-07 00:51:41 +0530

    Update Problem-04.py

--- Practice Sheet 07/Problem-04.py
+++ Practice Sheet 07/Problem-04.py
@@ -1,3 +1,7 @@
+'''
+Sort a tuple using second element tuple1 = (('b', 37),('c', 11),('d',29),('a', 23))
+'''
+
 tuple1 = (('b', 37), ('c', 11), ('d', 29), ('a', 23))
 list1 = list((tuple1[i][1], tuple1[i][0]) for i in range(len(tuple1)))
 list1.sort()
