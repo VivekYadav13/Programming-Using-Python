9899c69a5da02ee066c339b759e1d0715674def9
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-06 10:04:10 +0530

    Create Problem-04.py

---
+++ Practice Sheet 07/Problem-04.py
@@ -0,0 +1,5 @@
+tuple1 = (('b', 37), ('c', 11), ('d', 29), ('a', 23))
+list1 = list((tuple1[i][1], tuple1[i][0]) for i in range(len(tuple1)))
+list1.sort()
+sortedtuple1 = tuple((list1[i][1], list1[i][0]) for i in range(len(list1)))
+print(sortedtuple1)
