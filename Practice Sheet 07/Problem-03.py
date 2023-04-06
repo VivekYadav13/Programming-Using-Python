635f5cbb29a6a9fe799186c237a5390455beb1f1
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-07 00:51:09 +0530

    Update Problem-03.py

--- Practice Sheet 07/Problem-03.py
+++ Practice Sheet 07/Problem-03.py
@@ -1,3 +1,7 @@
+'''
+Sort a tuple using first element tuple1 = (('b', 37),('c', 11), ('d',29),('a', 23))
+'''
+
 tuple1 = (('b', 37), ('c', 11), ('d', 29), ('a', 23))
 list1 = list(tuple1)
 list1.sort()
