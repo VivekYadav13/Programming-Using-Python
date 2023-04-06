4fe58899ef4a0ad7a49718fd8a91ba5542c16343
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-06 16:46:20 +0530

    Create Problem-01.py

---
+++ Practice Sheet 06/Problem-01.py
@@ -0,0 +1,7 @@
+'''
+Find largest and smallest number in a list
+'''
+
+a = [5,6,7,2,9,10,4578,35,0]
+a.sort()
+print("Smallest and largest number from the list are", str(a[0]), "and", str(a[-1]), "respectively.")
