0dc025ce3c6e39cf08301a14c4f337e7a1cdaf57
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-11 15:09:14 +0530

    Update Problem-05.py

--- Practice Sheet 02/Problem-05.py
+++ Practice Sheet 02/Problem-05.py
@@ -4,9 +4,9 @@
 
 n = int(input("Enter number of rows for Pascal's triangle: "))
 for i in range(n):
-    print(' '*(n-i-1), end='')
+    print(' '*(n-i-1)*3, end='')
     c = 1
     for j in range(i+1):
-        print(c, end=' ')
+        print('{0:5}'.format(c), end=' ')
         c = int(c * (i - j) / (j + 1))
     print()
