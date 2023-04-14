417de6e649892d6e95935f5f9b320fa48e5e5f7a
Soham-Wani <68885526+Soham-Wani@users.noreply.github.com>
2023-04-14 14:48:01 +0530

    Update Problem-02.py

--- Practice Sheet 02/Problem-02.py
+++ Practice Sheet 02/Problem-02.py
@@ -1,5 +1,5 @@
 '''
-
+The numerical values of coins have been arranged so that the greedy algorithm will result in the smallest number of coins when making change. This means that the largest valued coin is tried first, and as many of those coins are used as possible. Then the next smaller denomination coin is used, and so on until the pennies are dealt out. So for 84 cents in change, a 50-cent piece could be used (leaving 34 cents), then a 25- cent piece (leaving 9 cents), a 5 cent piece (leaving 4 cents), and 4 pennies. If no 50- cent piece was available, then 25-cent pieces would be used in its place: 3 quarters, followed by a nickel and four pennies. Write a program that reads a number between 1 and 99 that is an amount of change to be given and prints the coin values that would be used.
 '''
 
 from random import randint
